77a8e70b36a577cd11bb74c377bd37bb38f1c110
Shivangik01 <shivangi.kochrekar@spit.ac.in>
2020-09-15 18:33:01 +0530

    resolved conflicts

--- lib/screens/home/feed.dart
+++ lib/screens/home/feed.dart
@@ -5,60 +5,49 @@ class Feed extends StatefulWidget {
   @override
   _FeedState createState() => _FeedState();
 }
-  
-  class _FeedState extends State<Feed> {
-    void response(query) async{
-      AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/cheer-a319f-7574a652fe56.json")
-      .build();
-      Dialogflow dialogflow = Dialogflow(authGoogle: authGoogle, language:Language.english);      
-      AIResponse aiResponse = await dialogflow.detectIntent(query);
-      setState(() {
+
+class _FeedState extends State<Feed> {
+  void response(query) async {
+    AuthGoogle authGoogle =
+        await AuthGoogle(fileJson: "assets/cheer-a319f-7574a652fe56.json")
+            .build();
+    Dialogflow dialogflow =
+        Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse aiResponse = await dialogflow.detectIntent(query);
+    setState(() {
       messsages.insert(0, {
         "data": 0,
         "message": aiResponse.getListMessage()[0]["text"]["text"][0].toString()
       });
     });
-    }
+  }
 
-    final messageInsert = TextEditingController();
+  final messageInsert = TextEditingController();
   List<Map> messsages = List();
-  
-  @override 
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      bottomNavigationBar: Row(
-        children:<Widget>
-        [
-          Container(
-            height: 60,
-            width: MediaQuery.of(context).size.width/3,
-            decoration: BoxDecoration(
-              color: Colors.orange
-            ),
-            child: Icon(Icons.home),
-          ),
-          Container(
-            height: 60,
-            width: MediaQuery.of(context).size.width/3,
-            decoration: BoxDecoration(
-              color: Colors.orange
-            ),
-            child: Icon(Icons.favorite),
-          ),
-          Container(
-            height: 60,
-            width: MediaQuery.of(context).size.width/3,
-            decoration: BoxDecoration(
-              color: Colors.orange
-            ),
-            child: Icon(Icons.chat),
-          )
-
-        ] 
-
-      ),
-
+      bottomNavigationBar: Row(children: <Widget>[
+        Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width / 3,
+          decoration: BoxDecoration(color: Colors.orange),
+          child: Icon(Icons.home),
+        ),
+        Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width / 3,
+          decoration: BoxDecoration(color: Colors.orange),
+          child: Icon(Icons.favorite),
+        ),
+        Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width / 3,
+          decoration: BoxDecoration(color: Colors.orange),
+          child: Icon(Icons.chat),
+        )
+      ]),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -66,7 +55,6 @@ class Feed extends StatefulWidget {
           style: TextStyle(color: Colors.black),
         ),
         backgroundColor: Colors.orange,
-        
       ),
       body: Container(
         child: Column(
@@ -98,9 +86,7 @@ class Feed extends StatefulWidget {
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 4.0),
                     child: IconButton(
-                      
                         icon: Icon(
-                          
                           Icons.send,
                           size: 30.0,
                           color: Colors.deepOrange,
@@ -129,46 +115,48 @@ class Feed extends StatefulWidget {
       ),
     );
   }
-<<<<<<< HEAD
+
   Widget chat(String message, int data) {
     // ignore: non_constant_identifier_names
     var BubbleNip;
-        return Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Bubble(
-                    radius: Radius.circular(15.0),
-                    color: data == 0 ? Colors.deepOrange : Colors.orangeAccent,
-                    elevation: 0.0,
-                    alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
-                    nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
-                child: Padding(
-                  padding: EdgeInsets.all(2.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage: AssetImage(
-                            data == 0 ? "assets/bot.png" : "assets/user.png"),
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Flexible(
-                          child: Text(
-                        message,
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ))
-                    ],
-                  ),
-                )),
-          );
-        }
-      
-        // ignore: non_constant_identifier_names
-        Bubble({Radius radius, ColorSwatch<int> color, double elevation, Alignment alignment, nip, Padding child}) {}
-}
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Bubble(
+          radius: Radius.circular(15.0),
+          color: data == 0 ? Colors.deepOrange : Colors.orangeAccent,
+          elevation: 0.0,
+          alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
+          nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
+          child: Padding(
+            padding: EdgeInsets.all(2.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                      data == 0 ? "assets/bot.png" : "assets/user.png"),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Flexible(
+                    child: Text(
+                  message,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ))
+              ],
+            ),
+          )),
+    );
+  }
 
-=======
+  // ignore: non_constant_identifier_names
+  Bubble(
+      {Radius radius,
+      ColorSwatch<int> color,
+      double elevation,
+      Alignment alignment,
+      nip,
+      Padding child}) {}
 }
->>>>>>> 8550ad8c5e028b1364d510218e4397c59e0fe89e
