41c15e41a191d0b8d0c07535a9c5ffda5d348f09
Shivangik01 <shivangi.kochrekar@spit.ac.in>
2020-09-16 15:26:09 +0530

    user details

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -16,34 +16,6 @@ class AuthService {
         .map(_userFromFirebaseUser);
   }
 
-  //login with email and passwd
-  Future logInWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      FirebaseUser user = result.user;
-
-      return _userFromFirebaseUser(user);
-    } catch (error) {
-      print(error.toString());
-      return null;
-    }
-  }
-
-  //register with email and passwd
-  Future registerWithEmailAndPassword(String email, String password) async {
-    try {
-      AuthResult result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      FirebaseUser user = result.user;
-
-      return _userFromFirebaseUser(user);
-    } catch (error) {
-      print(error.toString());
-      return null;
-    }
-  }
-
   //sign out
   Future signOut() async {
     try {
--- lib/services/database.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Databases {
-  final Firestore _firestore = Firestore.instance;
-}
