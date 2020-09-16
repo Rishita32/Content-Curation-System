67d1d75db47ccc370abd8a1f843a60c1d50f0efd
Neha <kaleneha997@gmail.com>
2020-09-16 20:14:43 +0530

    Routing Fixed

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,16 +1,15 @@
-import 'package:CheerApp/models/authModel.dart';
+import 'package:CheerApp/models/user.dart';
 import 'package:CheerApp/screens/authenticate/splash.dart';
-import 'package:CheerApp/screens/home/chatbot.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:CheerApp/screens/home/feed.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<AuthModel>(context);
+    final user = Provider.of<User>(context);
     print('wrapper');
     print(user);
-
     // return either the Home or Authenticate widget
     if (user == null) {
       return Splash();
