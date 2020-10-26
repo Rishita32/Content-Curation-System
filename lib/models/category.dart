141b06671fc1a964b787a998be41dc6f2719533c
Rishita32 <rishitamote@gmail.com>
2020-10-26 15:31:43 +0530

    feed

---
+++ lib/models/category.dart
@@ -0,0 +1,53 @@
+
+class Category
+{
+  String category; 
+  String type;
+  String author;
+  String title;
+  String content; 
+  String description;
+  String imageUrl;
+  String url;
+
+  // Category( 
+  //   {
+  //     this.author,
+  //     this.category,
+  //     this.content,
+  //     this.description,
+  //     this.imageUrl,
+  //     this.title,
+  //     this.type,
+  //     this.url
+  //   }
+
+  // );
+
+  Category.fromMap(Map<String, dynamic> data){
+    category=data['category'];
+    type=data['type'];
+    author=data['author'];
+    title=data['title'];
+    content=data['content'];
+    description=data['description'];
+    imageUrl=data['imageUrl'];
+    url=data['url'];
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      'category':category,
+      'type': type,
+      'author': author,
+      'title':title,
+      'content':content,
+      'description': description,
+      'imageUrl': imageUrl,
+      'url': url
+
+      
+
+    };
+  }
+}
--- lib/screens/authenticate/splash.dart
+++ lib/screens/authenticate/splash.dart
@@ -39,6 +39,14 @@ class StartState extends State<Splash>
           children: <Widget>[  
             Container( 
               child: Image.asset("assets/cheer.png"),
+              decoration: BoxDecoration(  
+                shape: BoxShape.circle,
+                color: Colors.orange,
+              
+
+              ),
+              margin: EdgeInsets.all(55.0),
+              padding: EdgeInsets.all(55.0),
             
             ),
             Padding(padding: EdgeInsets.only(top: 20.0)),
